9a01f349b3709dfdbd476d2245b6a035608ed8ea
Kilian Evang <noreply@texttheater.net>
2023-11-07 13:59:54 +0100

    Initial commit (untested code)

---
+++ README.md
@@ -0,0 +1,4 @@
+VerBot
+======
+
+Posts recent changes from a MediaWiki to a Simple Machines Forum.
---
+++ post.py
@@ -0,0 +1,76 @@
+import hashlib
+import urllib.parse
+
+
+import feedparser
+import mechanicalsoup
+import mysql.connector
+
+
+import config
+
+
+def wikiurl(title):
+    title = title.replace(' ', '_')
+    title = '/'.join(urllib.parse.quote(p) for p in title.split('/'))
+    return title
+
+
+def message(entry):
+    author = entry.author
+    author_url = f'https://neutsch.org/Benutzer:{wikiurl(author)}'
+    title = entry.title
+    title_url = f'https://neutsch.org/{wikiurl(title)}'
+    change_url = entry.link
+    return f'[url={author_url}]{author}[/url] ' \
+        f'hat folgende [url={change_url}]Änderung[/url] ' \
+        f'an der Seite [url={title_url}]title[/url] vorgenommen:\n\n' \
+        f'[html]{entry.description}[/html]'
+
+
+def post(b, entry):
+    b.open(config.forum_url_post)
+    b.select_form('#postmodify')
+    b['subject'] = entry.title
+    b['message'] = message(entry)
+    b['preview'] = None
+    b.submit_selected()
+
+
+if __name__ == '__main__':
+    # feed paresr
+    d = feedparser.parse(config.feed_url)
+    # forum browser
+    b = mechanicalsoup.StatefulBrowser()
+    b.open(config.forum_url_login)
+    b.select_form('#frmLogin')
+    b['user'] = config.forum_user
+    b['passwrd'] = config.forum_pass
+    b.submit_selected()
+    # database
+    db = mysql.connector.connect(
+        user=config.db_user,
+        password=config.db_pass,
+        host=config.db_host,
+        database=config.db_name,
+    )
+    cursor = db.cursor()
+    for entry in d.entries[::-1]:
+        print(message(entry))
+        guid = entry.id
+        id_hash = hashlib.sha256(guid.encode('utf-8')).hexdigest()
+        cursor.execute('''
+            SELECT COUNT(*)
+            FROM posted_entries
+            WHERE id = %s''',
+            (id_hash,)
+        )
+        count = cursor.fetchall()[0][0]
+        if not count:
+            post(b, entry)
+            cursor.execute('''
+                INSERT INTO posted_entries (id)
+                VALUES (%s)''',
+                (id_hash,)
+            )
+    db.commit()
